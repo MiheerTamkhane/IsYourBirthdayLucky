17fa6a19541187701058e333fd3a19f6ec851c04
Miheer Vilas Tamkhane <sarangtamkhane10@gmail.com>
2021-07-24 16:28:43 +0530

    complete UI

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const date = document.getElementById("date");
 const button = document.getElementById("submit");
 const lNumber = document.getElementById("number");
 const output = document.getElementById("output");
+const cancelBtn = document.getElementById("cancel-btn");
+const privacy = document.getElementById("privacy-notice");
+const footerPrivacy = document.getElementById("footer-privacy");
 
+//Submit btn click handler for checking lucky birthdate
 function clickHandler() {
   const userDate = date.value.split("-");
 
@@ -16,13 +20,35 @@ function clickHandler() {
   const luck = year + month + day;
   const number = lNumber.value;
 
-  if (luck % number === 0) {
+  if (year !== number) {
+    alert("please enter date");
+  } else if (number === "") {
+    alert("please fill lucky number");
+  } else if (luck % number === 0) {
     console.log("Your Birthdate is lucky");
-    output.innerHTML = "Your Birthdate is lucky";
+    output.innerHTML = "./lucky.html";
   } else {
     console.log("Your Birthdate is not Lucky");
-    output.innerHTML = "Your Birthdate is not Lucky";
+    output.innerHTML = `<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat deserunt
+    sequi tempora sapiente exercitationem non excepturi, assumenda, accusamus
+    qui sunt quam quo ullam id impedit rerum nobis esse a reiciendis est
+    libero doloribus nulla.
+  </p>`;
   }
 }
+//function for showing privacy notice
+function resetHandler(e) {
+  e.preventDefault();
+  privacy.innerText = `Privacy Notice! This Web does not store any data of user, Its totally
+    for fun.`;
+}
+//function for erasing privacy notice
+function cancelHandler() {
+  privacy.innerText = "";
+}
 
+//Events
 button.addEventListener("click", clickHandler);
+cancelBtn.addEventListener("click", cancelHandler);
+footerPrivacy.addEventListener("click", resetHandler);
